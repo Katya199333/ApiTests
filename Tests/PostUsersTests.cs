8a9a94d54883b60d023c3ecdb720c962752cddb6
katya199333 <bekmacheva@yandex.ru>
2022-02-10 14:13:34 +0300

    TestMaxima

---
+++ Tests/PostUsersTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestMaxima.models;
+
+namespace TestMaxima
+{
+
+
+    public class PostUsersTests
+    {
+        private const string Host = "https://reqres.in/api";
+        private const string Api = "/users";
+
+        [Test]
+        public async Task CheckPostUsersTesting()
+        {
+            var baseAddress = Host + Api;
+            var client = new HttpClient();
+            var strBody = "{\"name\": \"morpheus\" }";
+            var contentBody = new StringContent(strBody, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(baseAddress, contentBody);
+
+            var statusCode = response.StatusCode;
+            Assert.AreEqual(HttpStatusCode.Created, statusCode, $"Ответ от api {Api} не соответствует ожидаемому");
+        }
+
+        [Test]
+        public async Task CheckPostUsersByJsonTesting()
+        {
+            var baseAddress = Host + Api;
+            var client = new HttpClient();
+            var request = new UsersRequest()
+            {
+                Name = "morpheus"
+            };
+            var response = await client.PostAsJsonAsync(baseAddress, request);
+
+            var statusCode = response.StatusCode;
+            Assert.AreEqual(HttpStatusCode.Created, statusCode, $"Ответ от api {Api} не соответствует ожидаемому");
+        }
+
+
+    }
+
+}
---
+++ models/UsersRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace TestMaxima.models;
+
+public class UsersRequest
+{
+    public string Name { get; set; }
+}
